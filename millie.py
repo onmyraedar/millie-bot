3980ae23f1f93e9b9295a33fe27f17be15e5e5ed
Reanna Ishmael <rishmael@fordham.edu>
2022-05-01 03:03:09 -0400

    Added embed to info command

--- millie.py
+++ millie.py
@@ -18,6 +18,19 @@ async def on_ready():
 @bot.command()
 async def info(ctx):
     """Provides the author with info about Millie."""
-    await ctx.send("Hi, I'm Millie! I help artists combat art theft.")
+    info = discord.Embed(
+        title="Hi, I'm Millie! I'm here to help artists combat art theft.",
+        description="""
+        As a digital artist, it can be incredibly frustrating when someone steals your work and tries to profit off of it.
+        Thankfully, there's a law called the Digital Millenium Copyright Act (DMCA) that can help you get your stolen content removed.
+        Many websites have __online forms__ for submitting your DMCA takedown notice.
+        If such a form doesn't exist, you can __contact the site's DMCA agent directly__ or __reach out to the web host or ISP__.
+
+        Type **!email** to learn more about DMCA takedown emails.
+        Type **!forms** for help with DMCA forms for a particular site.
+        """,
+        color=0xe6c9ff
+        )
+    await ctx.send(embed=info)
 
 bot.run(token)
